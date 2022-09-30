b72898d2bef85ef3d3880334798306a8430de57d
freddykaberuka <freddykaberuka2009@gmail.com>
2022-09-30 19:21:53 +0200

    add lists of books

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,6 +1,9 @@
 const ADD_BOOK = 'bookstore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
-const books = [];
+const books = [{ id: 1, author: 'Michelle de Kretser', title: 'SCARY MONSTERS' },
+  { id: 2, author: 'Hernan Diaz', title: 'TRUST' },
+  { id: 3, author: 'Susan Straight', title: 'MECCA' },
+  { id: 4, author: 'Lindsey Fitzharris ', title: 'THE FACEMAKER' }];
 
 export const AddBookFunc = (NewBook) => ({
   type: ADD_BOOK,

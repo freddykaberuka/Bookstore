2d397fae28f348136f2254034ef1a385f2b562b4
freddykaberuka <freddykaberuka2009@gmail.com>
2022-09-30 18:40:58 +0200

    fix reviewer feedback

--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -1,4 +1,4 @@
-const CHECK_STATUS = 'CHECK_STATUS';
+const CHECK_STATUS = 'bookstore/categories/CHECK_STATUS';
 
 const categories = [];
 
@@ -9,7 +9,7 @@ export const CheckStatusFunc = () => ({
 const CategoriesReducer = (state = categories, action) => {
   switch (action.type) {
     case CHECK_STATUS:
-      return ['Under construction'];
+      return 'Under construction';
     default: return state;
   }
 };
